a2c579f4bec96739cfbf554f520fb569ee2a4ad6
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-24 23:06:13 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -1,1 +1,67 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int n, m;
+
+	cout << "Введите размер матрицы (M * N): ";
+	cin >> m >> n;
+
+	int **a = new int*[m];
+
+	for (int i = 0; i < m; i++)
+	{
+		a[i] = new int[n];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		cout << "Введите элементы " << i << " строки" << endl;
+		for (int j = 0; j < n; j++)
+		{
+			cout << "Введите элемент: ";
+			cin >> a[i][j];
+		}
+	}
+
+	cout << "Теущая матрица: " << endl;
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << a[i][j] << ' ';
+		}
+		cout << endl;
+	}
+
+	for (int i = m - 1; i >= 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (a[j][0] > a[j + 1][0])
+			{
+				for (int k = 0; k < n; k++)
+				{
+					int temp = a[j][k];
+					a[j][k] = a[j + 1][k];
+					a[j + 1][k] = temp;
+				}
+			}
+		}
+	}
+
+	cout << "Новая матрица: " << endl;
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << a[i][j] << ' ';
+		}
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
